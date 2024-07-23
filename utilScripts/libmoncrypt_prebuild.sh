9bc971aebfbd8ff4dcaaef4a07831142fdf309fa
NicoLongfield <lonn1001@usherbrooke.ca>
2024-07-23 16:14:02 -0400

    Libmongocrypt fix

---
+++ utilScripts/libmoncrypt_prebuild.sh
@@ -0,0 +1,11 @@
+git clone --branch node-v6.0.1 https://github.com/mongodb/libmongocrypt.git
+cd libmongocrypt
+
+# Build libmongocrypt node bindings
+cd bindings/node
+
+bash ./etc/build-static.sh
+
+cd ../../..
+# Copy and overwrite the existing node bindings
+cp -r ./libmongocrypt/bindings/node/ ./node_modules/mongo-client-encryption
